cff675bc20faf5bf1566cc3b32ecb4ee660fd5b4
arcii <noliugaaa@gmail.com>
2023-10-16 12:05:21 +0800

    parameterized queries to prevent SQL injections

--- Form1.cs
+++ Form1.cs
@@ -51,57 +51,111 @@ namespace WindowsFormWithCRUD
         private void btnUPDATE_Click(object sender, EventArgs e)
         {
             string connection = "server=localhost; user id=root; password=password; database=mydb";
-            string querry = "UPDATE new_table SET first_name = '"+this.first_name.Text+ "', last_name = '"+this.last_name.Text+ "',phone = '"+this.phone.Text+ "',email = '"+this.email.Text+ "' WHERE id = '"+this.ID.Text+"' ";
-            MySqlConnection conn = new MySqlConnection(connection);
-            MySqlCommand cmd = new MySqlCommand(querry, conn);
-            conn.Open();
-            _ = cmd.ExecuteReader();
-            MessageBox.Show("Data was updated.");
-            conn.Close();
+            string query = "UPDATE new_table SET first_name = @FirstName, last_name = @LastName, phone = @Phone, email = @Email WHERE id = @ID";
+
+            using (MySqlConnection conn = new MySqlConnection(connection))
+            using (MySqlCommand cmd = new MySqlCommand(query, conn))
+            {
+                conn.Open();
+
+                // Use parameterized queries to prevent SQL injection
+                cmd.Parameters.AddWithValue("@FirstName", this.first_name.Text);
+                cmd.Parameters.AddWithValue("@LastName", this.last_name.Text);
+                cmd.Parameters.AddWithValue("@Phone", this.phone.Text);
+                cmd.Parameters.AddWithValue("@Email", this.email.Text);
+                cmd.Parameters.AddWithValue("@ID", this.ID.Text);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Data was updated.");
+                }
+                else
+                {
+                    MessageBox.Show("Data Update Failed.");
+                }
+            }
         }
+
 
         private void btnADD_Click(object sender, EventArgs e)
         {
             string connection = "server=localhost; user id=root; password=password; database=mydb";
-            string querry = "INSERT INTO new_table" +
-                "(first_name, last_name, phone, email)" +
-                "VALUES(" +
-                "'"+this.first_name.Text+ "'," +
-                "'"+this.last_name.Text+ "'," +
-                "'"+this.phone.Text+ "'," +
-                "'"+this.email.Text+"')";
-            MySqlConnection conn = new MySqlConnection(connection);
-            MySqlCommand cmd = new MySqlCommand(querry, conn);
-            conn.Open();
-            _ = cmd.ExecuteReader();
-            MessageBox.Show("Data Successfully Added.");
-            conn.Close();
+            string query = "INSERT INTO new_table (first_name, last_name, phone, email) " +
+                           "VALUES (@FirstName, @LastName, @Phone, @Email)";
+
+            using (MySqlConnection conn = new MySqlConnection(connection))
+            using (MySqlCommand cmd = new MySqlCommand(query, conn))
+            {
+                conn.Open();
+
+                // Use parameterized queries to prevent SQL injection
+                cmd.Parameters.AddWithValue("@FirstName", this.first_name.Text);
+                cmd.Parameters.AddWithValue("@LastName", this.last_name.Text);
+                cmd.Parameters.AddWithValue("@Phone", this.phone.Text);
+                cmd.Parameters.AddWithValue("@Email", this.email.Text);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Data Successfully Added.");
+                }
+                else
+                {
+                    MessageBox.Show("Data Insertion Failed.");
+                }
+            }
         }
+
 
         private void btnDELETE_Click(object sender, EventArgs e)
         {
             string connection = "server=localhost; user id=root; password=password; database=mydb";
-            string querry = "DELETE FROM new_table WHERE id='"+this.ID.Text+"' ";
-            MySqlConnection conn = new MySqlConnection(connection);
-            MySqlCommand cmd = new MySqlCommand(querry, conn);
-            conn.Open();
-            _ = cmd.ExecuteReader();
-            MessageBox.Show("Data has been deleted.");
-            conn.Close();
+            string query = "DELETE FROM new_table WHERE id = @ID";
+
+            using (MySqlConnection conn = new MySqlConnection(connection))
+            using (MySqlCommand cmd = new MySqlCommand(query, conn))
+            {
+                conn.Open();
+
+                // Use parameterized queries to prevent SQL injection
+                cmd.Parameters.AddWithValue("@ID", this.ID.Text);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Data has been deleted.");
+                }
+                else
+                {
+                    MessageBox.Show("Data Deletion Failed.");
+                }
+            }
         }
+
 
         private void btnLoadData_Click(object sender, EventArgs e)
         {
             string connection = "server=localhost; user id=root; password=password; database=mydb";
-            string querry = "SELECT * FROM mydb.new_table";
-            MySqlConnection conn = new MySqlConnection(connection);
-            MySqlCommand cmd = new MySqlCommand(querry, conn);
-            MySqlDataAdapter da = new MySqlDataAdapter();
-            da.SelectCommand = cmd;
-            DataTable dt = new DataTable();
-            da.Fill(dt);
-            dataGridView1.DataSource = dt;
+            string query = "SELECT * FROM new_table";
+
+            using (MySqlConnection conn = new MySqlConnection(connection))
+            using (MySqlCommand cmd = new MySqlCommand(query, conn))
+            {
+                conn.Open();
+
+                using (MySqlDataAdapter da = new MySqlDataAdapter(cmd))
+                {
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                }
+            }
         }
+
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
